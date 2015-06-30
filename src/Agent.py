d87e1c57dd2b643f362b3e2c9a73699306ed8016
Ofra Amir <oamir@seas.harvard.edu>
2015-06-30 10:00:55 -0400

    choose action before major fix (check all nodes, not just all colors for node

--- src/Agent.py
+++ src/Agent.py
@@ -22,6 +22,14 @@ class Agent:
     #the function updates the agents' knowledge about the graph
     #nodesColorsList is a dict of node_id and current color
     def updateBelief(self, nodesColorsList):
+        #reset old stuff
+        for node, data in self.knownGraph.nodes(data = True):
+            if data['uptoDate']== True: # if node was uptoDate in the last turn, treat the color unchanged but reset uptoDate so next turn it gets reset
+                data['uptoDate']= False
+            else: #the node is not uptodate, reset the color to unknown
+                data['color'] = -1
+        
+        #update new stuff
         for node,color in nodesColorsList.iteritems():
             self.knownGraph[node]['color'] = color
             self.knownGraph[node]['uptoDate'] = True
@@ -35,30 +43,43 @@ class Agent:
         
         return newColors;
     
-    def chooseActionsRecur(self, actionSet, numConflicts, nodeCounter, bestSolution):
+    def chooseActionsRecur(self, currSolution, nodeCounter, bestSolution):
+        #TODO: add caching of partial action sets as to not recompute stuff...
         #stop condition - when we reached the limit of actions permitted, or when reached the last node we can change
-        if ((len(actionSet) == self.actionLimit) | (nodeCounter == len(self.controlledNodes))):
-            if numConflicts < bestSolution['conflicts']: 
-                bestSolution['actionSet']=actionSet
-                bestSolution['conflicts']=numConflicts
+        if ((len(currSolution['actionSet']) == self.actionLimit) | (nodeCounter == len(self.controlledNodes))):
+            if currSolution['conflicts'] < bestSolution['conflicts']: 
+                bestSolution=currSolution
+            #break ties in favor of more known non-conflicts (otherwise might bias towards doing nothing)   
+            elif currSolution['conflicts'] == bestSolution['conflicts']:
+                if currSolution['notConflicts'] > bestSolution['notConflicts']:
+                    bestSolution=currSolution
             return
         else:
             #call function with all possible options for next node (not change, change to each of the colors that differ from the current color)
-            self.chooseActionsRecur(self, actionSet,numConflicts,nodeCounter+1,bestSolution) #don't include a change to this node in action set
+            self.chooseActionsRecur(self, currSolution,nodeCounter+1,bestSolution) #don't include a change to this node in action set
             for color in self.colors:
                 if color != self.knownGraph[self.controlledNodes[nodeCounter]]['color']:
-                    newActionSet = copy.deepcopy(actionSet)
+                    newActionSet = copy.deepcopy(currSolution['actionSet'])
                     newActionSet.append((nodeCounter,color))
-                    newNumConflicts = self.computeNumConflicts(newActionSet)
-                    if newNumConflicts>bestSolution['conflicts']:
+                    newGraphState = self.computeNumConflicts(newActionSet)
+                    if newGraphState['conflicts']>bestSolution['conflicts']:
                         continue;
+
                     else:
-                        self.chooseActionsRecur(self, newActionSet,newNumConflicts,nodeCounter+1,bestSolution) #call function to check this action set
+                        newSolution = {}
+                        newSolution['actionSet'] = newActionSet
+                        newSolution['conflicts'] = newGraphState['conflicts']
+                        newSolution['notConflicts'] = newGraphState['notConflicts']
+                        newSolution['unknown'] = newGraphState['unknown']
+                        self.chooseActionsRecur(self, newSolution,nodeCounter+1,bestSolution) #call function to check this action set
                         
         return;
                     
                     
-    def computeNumConflicts(self,actionSet): #TODO: implement
+    def computeNumConflicts(self,actionSet): #TODO: implement; return dictionary with 'conflicts' 'notConflicts' and 'unknown'
+        prevColors = {}
+        for action in actionSet:
+            
         
         pass
                 
---
+++ src/Utils.py
@@ -0,0 +1,14 @@
+'''
+Created on Jun 11, 2015
+
+@author: Ofra
+'''
+
+#'''
+#Gets a networkx graph object in which each node has a color attribute
+#'''
+#def computeNumberOfConflictsInGraph(graph):
+#    pass
+    
+if __name__ == '__main__':
+    pass
